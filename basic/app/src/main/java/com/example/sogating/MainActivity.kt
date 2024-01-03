60c64cbbdcf860a0a06b49545184808cfb36d913
김다슬 <daseul727@hankookn.com>
2024-01-03 16:01:59 +0900

    CardStackAdapter - mainActivity 연결

--- basic/app/src/main/java/com/example/sogating/MainActivity.kt
+++ basic/app/src/main/java/com/example/sogating/MainActivity.kt
@@ -2,13 +2,66 @@ package com.example.sogating
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.sogating.slider.CardStackAdapter
 import com.yuyakaido.android.cardstackview.CardStackLayoutManager
+import com.yuyakaido.android.cardstackview.CardStackListener
+import com.yuyakaido.android.cardstackview.CardStackView
+import com.yuyakaido.android.cardstackview.Direction
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var cardStackAdapter: CardStackAdapter
+    lateinit var manager : CardStackLayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val cardStackView = findViewById<CardStackView>(R.id.cardStackView)
+
+
+        /*
+        1. object 마우스 커서 두고 override
+        manager = CardStackLayoutManager(baseContext, object : CardStackListener {
+
+        })*/
+        manager = CardStackLayoutManager(baseContext, object : CardStackListener {
+            override fun onCardDragging(direction: Direction?, ratio: Float) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onCardSwiped(direction: Direction?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onCardRewound() {
+                TODO("Not yet implemented")
+            }
+
+            override fun onCardCanceled() {
+                TODO("Not yet implemented")
+            }
+
+            override fun onCardAppeared(view: View?, position: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onCardDisappeared(view: View?, position: Int) {
+                TODO("Not yet implemented")
+            }
+
+        })
+
+        val testList = mutableListOf<String>()
+        testList.add("a")
+        testList.add("b")
+        testList.add("c")
+
+        //이 부분이 CardStackAdapter 에서 들어오는 파라미터
+        cardStackAdapter = CardStackAdapter(baseContext,testList)
+        cardStackView.layoutManager = manager
+        cardStackView.adapter = cardStackAdapter
+
     }
 }
