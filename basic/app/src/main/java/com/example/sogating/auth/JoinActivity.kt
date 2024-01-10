c22ce3c301f710b90c206ff6195b93d754fcd426
김다슬 <daseul727@gmail.com>
2024-01-10 15:38:58 +0900

    [회원가입] firebase 연결 회원가입 처리

--- basic/app/src/main/java/com/example/sogating/auth/JoinActivity.kt
+++ basic/app/src/main/java/com/example/sogating/auth/JoinActivity.kt
@@ -3,12 +3,54 @@ package com.example.sogating.auth
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
+import com.example.sogating.MainActivity
 import com.example.sogating.R
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class JoinActivity : AppCompatActivity() {
+
+    private val TAG = "JoinActivity"
+
+    private lateinit var auth: FirebaseAuth
+
+    /*
+        참고 : https://firebase.google.com/docs/auth/android/start?hl=ko#kotlin+ktx
+    */
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join)
+
+        auth = Firebase.auth
+
+        val joinBtn = findViewById<Button>(R.id.joinSubmit)
+        joinBtn.setOnClickListener {
+
+            val email = findViewById<TextInputEditText>(R.id.emailArea)
+            val password = findViewById<TextInputEditText>(R.id.passwordArea)
+
+            //Log.d(TAG, email.text.toString())
+            //Log.d(TAG, password.text.toString())
+
+            auth.createUserWithEmailAndPassword(email.text.toString(), password.text.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Log.d(TAG, "createUserWithEmail:success")
+
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    }
+                }
+        }
     }
 }
