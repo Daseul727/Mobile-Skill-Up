094e17aeb77aa51c773ba53f168c4de84e8aab2f
김다슬 <daseul727@gmail.com>
2024-01-10 10:49:37 +0900

    intro 에서 join 버튼 클릭하여 join 페이지 이동

--- basic/app/src/main/java/com/example/sogating/auth/IntroActivity.kt
+++ basic/app/src/main/java/com/example/sogating/auth/IntroActivity.kt
@@ -1,12 +1,35 @@
 package com.example.sogating.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.sogating.R
 
 class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
+
+        //1. joinBtn 을 가져온다
+        /*
+            방법1
+            val joinBtn = findViewById<Button>(R.id.joinBtn)
+
+            방법2
+            val joinBtn : Button = findViewById(R.id.joinBtn)
+        */
+
+        val joinBtn = findViewById<Button>(R.id.joinBtn)
+
+        joinBtn.setOnClickListener {
+
+            //2. joinActivity 화면을 intent 에 넣고
+            val intent = Intent(this, JoinActivity::class.java)
+
+            //3. joinActivity 로 이동
+            startActivity(intent)
+        }
+
     }
 }
