489483a5397805cbdf829a3452017d7570e40ab3
김다슬 <daseul727@gmail.com>
2024-01-24 13:20:39 +0900

    [Main] firebase storage에 올린 이미지 가져오기

--- basic/app/src/main/java/com/example/sogating/slider/CardStackAdapter.kt
+++ basic/app/src/main/java/com/example/sogating/slider/CardStackAdapter.kt
@@ -4,10 +4,14 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.sogating.R
 import com.example.sogating.auth.UserDataModel
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 
 /*
 
@@ -68,11 +72,23 @@ class CardStackAdapter(val context: Context, val items: List<UserDataModel>) : R
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
+        val image = itemView.findViewById<ImageView>(R.id.profileImageArea)
         val nickname = itemView.findViewById<TextView>(R.id.itemNickname)
         val age = itemView.findViewById<TextView>(R.id.itemAge)
         val city = itemView.findViewById<TextView>(R.id.itemCity)
 
         fun binding(data : UserDataModel) {
+
+            //이미지
+            val storageRef = Firebase.storage.reference.child(data.uid  +".png")
+            storageRef.downloadUrl.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Glide.with(context)
+                        .load(task.result)
+                        .into(image)
+                }
+            }
+
             nickname.text = data.nickname
             age.text = data.age
             city.text = data.city
