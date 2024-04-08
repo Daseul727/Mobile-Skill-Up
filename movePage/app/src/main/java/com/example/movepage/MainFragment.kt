fe787f3eeaa7f49bfbecaa66df82399a51e36c33
김다슬 <daseul727@gmail.com>
2024-04-08 09:32:24 +0900

    movePage - add navigation graph

---
+++ movePage/app/src/main/java/com/example/movepage/MainFragment.kt
@@ -0,0 +1,43 @@
+package com.example.movepage
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.movepage.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    private var _binding: FragmentMainBinding? = null
+    private val binding: FragmentMainBinding
+        get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = DataBindingUtil.inflate(
+            layoutInflater, R.layout.fragment_main, container, false
+        )
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            btnMoveSecond.setOnClickListener {
+
+            }
+
+            btnMoveThird.setOnClickListener {
+
+            }
+        }
+    }
+
+}
