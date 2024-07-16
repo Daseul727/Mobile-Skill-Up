4a40f131fc950247367d42b9202ffad2aed610c0
김다슬 <daseul727@gmail.com>
2024-07-16 11:02:06 +0900

    Fitness - add bottom

---
+++ Fitness/app/src/main/java/com/example/fitness/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.fitness
+
+class Constants {
+    companion object {
+        val BOARD = "BOARD"
+        val MAIN = "MAIN"
+        val MYPAGE = "MYPAGE"
+    }
+
+}
---
+++ Fitness/app/src/main/java/com/example/fitness/ui/main/MainFragment.kt
@@ -0,0 +1,24 @@
+package com.example.fitness.ui.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.fitness.databinding.FragmentMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainFragment : Fragment() {
+
+    private var _binding: FragmentMainBinding? = null
+    private val binding: FragmentMainBinding
+        get() = _binding!!
+
+    companion object {
+        fun newInstance(): MainFragment {
+            val fragment = MainFragment()
+            val args = Bundle()
+            fragment.arguments = args
+            return fragment
+        }
+
+    }
+}
